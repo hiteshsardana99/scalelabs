8be0e195e988e6fd919150d6b0550b8ad87cd220
hiteshsardana99 <hiteshsardana99@gmail.com>
2018-09-29 12:19:35 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,9 @@
 
 module.exports = {
-  MongoURI : 'mongodb://hitesh:scale12345@ds147181.mlab.com:47181/scalelabs',
-  local : 'mongodb://127.0.0.1:27017/local',
-  secretKey : 'asgfshfiasdhfkshdfuihisdufhiusdhfsdifhsdjhfsdihsd',
-  sessionKey : 'diufhiugfifhsfiw',
-  googleClientId : '12823243640-66uundp48sp7kvmkqrqpt815umbpclc4.apps.googleusercontent.com',
-  googleClientSecret : 'w7U4nPn2dciGMqpaPg8jqjJ0'
+  MongoURI : '',
+  local : '',
+  secretKey : '',
+  sessionKey : '',
+  googleClientId : '',
+  googleClientSecret : ''
 };
